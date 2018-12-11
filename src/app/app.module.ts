cdfe142a5c708eb918c371913c00230ce53dd084
Jon Janelle <jon@nextgengolf.org>
2018-12-10 22:09:06 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
+import { AppRoutingModule }     from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CatComponent } from './cats/cat.component';
+import { HomeComponent } from './home/home.component'; 
+import { ResourcesComponent } from './resources/resources.component';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatRippleModule,
+        MatInputModule, MatTabsModule, MatTableModule, MatPaginatorModule, MatIconModule,
+        MatProgressSpinnerModule, MatDialogModule, MatSnackBarModule, MatAutocompleteModule,
+        MatSortModule} from '@angular/material';
+import { AddResourceDialog } from './resources/dialogs/add-resource-dialog';
+import { AddEventDialog } from './events/dialogs/add-event-dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatComponent, 
+    FooterComponent, 
+    HomeComponent,
+    NavbarComponent,
+    ResourcesComponent, 
+    AddResourceDialog,
+    AddEventDialog
   ],
+  entryComponents: [AddResourceDialog],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule, MatCheckboxModule, MatCardModule, MatPaginatorModule,
+    MatRippleModule, MatTabsModule, MatTableModule, MatInputModule, MatIconModule,
+    MatProgressSpinnerModule, MatDialogModule, MatSnackBarModule, MatAutocompleteModule, MatSortModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatRippleModule, 
+    MatPaginatorModule, MatTabsModule, MatTableModule, MatInputModule, MatIconModule,
+    MatProgressSpinnerModule, MatDialogModule, MatSnackBarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NavbarComponent, AppComponent, FooterComponent]
 })
 export class AppModule { }
